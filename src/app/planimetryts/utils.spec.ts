490084da3a85892fde5a82d145765ddc720865b7
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-01-11 12:35:18 +0100

    test(planimetrics/util): fix tests

--- src/app/planimetryts/utils.spec.ts
+++ src/app/planimetryts/utils.spec.ts
@@ -1,4 +1,24 @@
-import {Equation} from './util';
+import {Equation, radianToDegree, degreeToRadian} from './util';
+
+describe(`Angle conversion`, () => {
+
+    it(`should convert radians to degrees`, () => {
+        expect(radianToDegree(0)).toBeCloseTo(0);
+        expect(radianToDegree(1)).toBeCloseTo(57.2958);
+        expect(radianToDegree(-1)).toBeCloseTo(-57.2958);
+        expect(radianToDegree(3.14159)).toBeCloseTo(179.999848);
+        expect(radianToDegree(42)).toBeCloseTo(2406.42);
+    });
+
+    it(`should convert degrees to radians`, () => {
+        expect(degreeToRadian(0)).toBeCloseTo(0);
+        expect(degreeToRadian(1)).toBeCloseTo(0.0174533);
+        expect(degreeToRadian(-1)).toBeCloseTo(-0.0174533);
+        expect(degreeToRadian(180)).toBeCloseTo(3.14159);
+        expect(degreeToRadian(42)).toBeCloseTo(0.733038);
+    });
+
+});
 
 describe(`Equation`, () => {
 
@@ -35,9 +55,54 @@ describe(`Equation`, () => {
                 const D = Equation.Quadratic.getDiscriminant(1, -2, 1);
                 expect(D).toBeCloseTo(0);
             });
+
+            it(`impossible to solve`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, 0, 1);
+                expect(solutions).toBe(0);
+            });
+
+            it(`should throw if A = 0`, () => {
+                expect(() => Equation.Quadratic.getDiscriminant(0, 1, 2)).toThrow();
+            });
         });
 
-        it(`should get solutions for`, () => {
+        describe(`should get number of solutions for`, () => {
+            it(`non-zero A, B, C`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, -5, 6);
+                expect(solutions).toBe(2);
+            });
+
+            it(`C = 0`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, -2, 0);
+                expect(solutions).toBe(2);
+            });
+
+            it(`B = 0`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, 0, -4);
+                expect(solutions).toBe(2);
+            });
+
+            it(`B = C = 0`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, 0, 0);
+                expect(solutions).toBe(1);
+            });
+
+            it(`D = 0 and non-zero A, B, C`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, -2, 1);
+                expect(solutions).toBe(1);
+            });
+
+            it(`impossible to solve`, () => {
+                const solutions = Equation.Quadratic.numberOfSolutions(1, 0, 1);
+                expect(solutions).toBe(0);
+            });
+
+            it(`should throw if A = 0`, () => {
+                expect(() => Equation.Quadratic.numberOfSolutions(0, 1, 2)).toThrow();
+            });
+        });
+
+        describe(`should get solutions for`, () => {
             it(`non-zero A, B, C`, () => {
                 const solutions = Equation.Quadratic.solve(1, -5, 6);
                 expect(solutions.length).toBe(2);
@@ -72,8 +137,12 @@ describe(`Equation`, () => {
             });
 
             it(`impossible to solve`, () => {
-                const solutions = Equation.Quadratic.solve(1, 2, 1);
-                expect(solutions).toBe(null);
+                const solutions = Equation.Quadratic.solve(1, 0, 1);
+                expect(solutions).toEqual([]);
+            });
+
+            it(`should throw if A = 0`, () => {
+                expect(() => Equation.Quadratic.solve(0, 1, 2)).toThrow();
             });
         });
 
