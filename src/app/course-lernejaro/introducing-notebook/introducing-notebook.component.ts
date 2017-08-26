75e6aaf3d891e7a73ed2151c0b2fefcf788fdee9
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-08-26 13:34:54 +0200

    demo(notebook): add whole text

--- src/app/course-lernejaro/introducing-notebook/introducing-notebook.component.ts
+++ src/app/course-lernejaro/introducing-notebook/introducing-notebook.component.ts
@@ -1,17 +1,5 @@
 import {Component, OnInit} from '@angular/core'
-import {Point} from '../../planimetryts/geometry-objects/point'
-import {MaterialColor} from '../../planimetryts/geometry-objects/material-colors'
-import {GeometryObject} from '../../planimetryts/geometry-objects/geometry-object'
-
-interface ComplexAlgebraic {
-  realPart: number
-  imaginaryPart: number
-}
-
-interface ComplexEuler {
-  modulo: number
-  argument: number
-}
+import {format} from '../../code/code'
 
 @Component({
   selector: 'lrn-introducing-notebook',
@@ -20,31 +8,18 @@ interface ComplexEuler {
 })
 export class IntroducingNotebookComponent implements OnInit {
 
-  public complexNumber: ComplexAlgebraic = {
-    realPart: 1,
-    imaginaryPart: 2,
-  }
-
-  public algebraicToEuler(algebraic: ComplexAlgebraic): ComplexEuler {
-    const modulo = Math.hypot(algebraic.realPart, algebraic.imaginaryPart)
-    const argument = Math.atan2(algebraic.imaginaryPart, algebraic.realPart)
-    return {modulo, argument}
-  }
-
-  public eulerToAlgebraic(euler: ComplexEuler): ComplexAlgebraic {
-    const realPart = euler.modulo * Math.cos(euler.argument)
-    const imaginaryPart = euler.modulo * Math.sin(euler.argument)
-    return {realPart, imaginaryPart}
-  }
-
-  public pointA = Point.CENTER.label('A').strokeColor(MaterialColor.BLUE)
-  public pointB = Point.FromCartesianCoordinates(30, 20).label('B').strokeColor(MaterialColor.RED)
-
-  public interactivePoints = [this.pointA, this.pointB]
-  public objects: GeometryObject[] = []
-
-  public updateDiagram({interactivePoints: [A, B]}) {
-    console.log(A, B)
+  public structuralDirectives = {
+    noSugar: format`
+      <ng-template [ngIf]="!!user">
+        <span>Welcome, {{ user.name }}</span>
+      </ng-template>
+    `,
+    partialSugar: format`
+      <span template="ngIf !!user">Welcome, {{ user.name }}</span>
+    `,
+    microSyntax: format`
+      <span *ngIf="!!user">Welcome, {{ user.name }}</span>    
+    `,
   }
 
   constructor() {
