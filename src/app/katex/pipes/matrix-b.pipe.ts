6c089453944e5ebbe23f4dfd43eab873bf28c9ff
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-09-04 23:13:10 +0200

    docs(katex): add docs for pipes

--- src/app/katex/pipes/matrix-b.pipe.ts
+++ src/app/katex/pipes/matrix-b.pipe.ts
@@ -1,6 +1,14 @@
 import {Pipe, PipeTransform} from '@angular/core'
 import {KatexService} from '../katex.service'
 
+/**
+ * A pipe version of the bMatrix transformer found in the
+ * KaTeX service.
+ *
+ * Transform a JavaScript array of arrays (a matrix) into a
+ * string which represents a matrix with bracket parans in
+ * LaTeX notation.
+ */
 @Pipe({name: 'bMatrix'})
 export class BMatrixPipe implements PipeTransform {
 
