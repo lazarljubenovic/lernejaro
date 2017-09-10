a64a153273803b6712c1b8d66fb572da58ba8842
lazarljubenovic <lazar.ljubenovic.1995@gmail.com>
2017-09-10 09:05:08 +0200

    refactor(markdown): use format template tag

--- src/app/markdown/markdown-parser.service.ts
+++ src/app/markdown/markdown-parser.service.ts
@@ -1,53 +1,35 @@
 import {Injectable} from '@angular/core'
 import * as marked from 'marked'
+import {format} from '../code/code'
 
 @Injectable()
 export class MarkdownParserService {
 
-    private getFirstNonEmptyLine(text: string): string {
-        return text.split('\n').find(line => line !== '')
-    }
+  private encloseInTag(text: string,
+                       tagName: string,
+                       className: string,
+                       on: boolean = true): string {
+    return on ?
+      `<${tagName} class="${className}">${text}</${tagName}>` :
+      text
+  }
 
-    // Find the gobble automatically
-    private getGobble(text: string): number {
-        const firstLine: string = this.getFirstNonEmptyLine(text)
-        const spaces: string = firstLine.match(/(^\s*)/)[0]
-        return spaces.length
+  public transform(text: string): string {
+    if (text) {
+      text = text.replace(/&gt;/g, '>')
+      const gobbledText = format`${text}`
+      const markedText: string = marked(gobbledText)
+      return this.encloseInTag(markedText, 'div', 'slide-inner', false)
+    } else {
+      return ''
     }
+  }
 
-    private gobble(text: string): string {
-        const gobble = this.getGobble(text)
-        return text
-            .split('\n')
-            .map(line => line.slice(gobble))
-            .join('\n')
-    }
-
-    private encloseInTag(text: string,
-                         tagName: string,
-                         className: string,
-                         on: boolean = true): string {
-        return on ?
-            `<${tagName} class="${className}">${text}</${tagName}>` :
-            text
-    }
-
-    public transform(text: string): string {
-        if (text) {
-            text = text.replace(/&gt;/g, '>')
-            const gobbledText = this.gobble(text)
-            const markedText: string = marked(gobbledText)
-            return this.encloseInTag(markedText, 'div', 'slide-inner', false)
-        } else {
-            return ''
-        }
-    }
-
-    constructor() {
-        marked.setOptions({
-            smartypants: true,
-            gfm: true,
-        })
-    }
+  constructor() {
+    marked.setOptions({
+      smartypants: true,
+      gfm: true,
+    })
+  }
 
 }
